ccd7bbac7508923b788a4587c5abe68347a53170
raiizoor <mateus.informatica44@gmail.com>
2021-05-29 22:46:53 -0300

    Added dictionary with date from register items

---
+++ delivery/foods.py
@@ -0,0 +1,20 @@
+items = {
+    'name': 'Pastel',
+    'valor': 5.99,
+    'description': 'Carne Moída, Salsicha, Tomate, Batata Palha, Ketchup, Maionese.',
+},
+{
+    'name': 'Hamburguer',
+    'valor': 29,99,
+    'description': 'Pão brioche, 2 hambúrgueres smash de carne suína, creme de cheddar, cebola caramelizada e molho barbecue.'
+},
+{
+    'name': 'Sorvete',
+    'valor': 30,99,
+    'description': 'Pote de 950 ml com o nosso autêntico Sorvete de Chocolate e pedaços macios de Chocolate Trufado. O Chiquinho no Pote foi feito pra curtir enquanto assiste aquele filminho no aconchego do sofá.'
+},
+{
+    'name': 'Açai',
+    'valor': 19.99,
+    'description': 'Dois deliciosos copos de 480ml, recheados com Açaí natural + Granola + Leite condensado + Banana, tudo por um preço especial!'
+}
